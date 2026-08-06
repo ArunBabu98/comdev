eecc4e4106d07b86fdd0f11aea0048770bfd645e
Arun Babu <tenshilabs@Aruns-Mac-mini-2.local>
2026-08-07 01:01:22 +0530

    comdev bind, connect, send and rcv completed. - small communicating program implemented

--- src/sys.rs
+++ src/sys.rs
@@ -1,7 +1,7 @@
-// src/sys.rs
 use std::io;
 
-// --- UNIX ---
+// ==================== UNIX IMPLEMENTATION (macOS / Linux / BSD) ====================
+
 #[cfg(unix)]
 pub type RawSocket = libc::c_int;
 
@@ -24,7 +24,117 @@ pub fn close_socket(fd: RawSocket) {
     }
 }
 
-// --- WINDOWS ---
+#[cfg(unix)]
+pub fn bind_socket(fd: RawSocket, ip: &str, port: u16) -> io::Result<()> {
+    let ip_addr: std::net::Ipv4Addr = ip
+        .parse()
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid IP format"))?;
+
+    let mut sockaddr: libc::sockaddr_in = unsafe { std::mem::zeroed() };
+    sockaddr.sin_family = libc::AF_INET as libc::sa_family_t;
+    sockaddr.sin_port = port.to_be();
+    sockaddr.sin_addr = libc::in_addr {
+        s_addr: u32::from(ip_addr).to_be(),
+    };
+
+    let res = unsafe {
+        libc::bind(
+            fd,
+            &sockaddr as *const libc::sockaddr_in as *const libc::sockaddr,
+            std::mem::size_of::<libc::sockaddr_in>() as libc::socklen_t,
+        )
+    };
+
+    if res < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+#[cfg(unix)]
+pub fn listen_socket(fd: RawSocket, backlog: i32) -> io::Result<()> {
+    let res = unsafe { libc::listen(fd, backlog) };
+    if res < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+#[cfg(unix)]
+pub fn accept_socket(fd: RawSocket) -> io::Result<(RawSocket, String)> {
+    let mut client_addr: libc::sockaddr_in = unsafe { std::mem::zeroed() };
+    let mut addr_len = std::mem::size_of::<libc::sockaddr_in>() as libc::socklen_t;
+
+    let client_fd = unsafe {
+        libc::accept(
+            fd,
+            &mut client_addr as *mut libc::sockaddr_in as *mut libc::sockaddr,
+            &mut addr_len,
+        )
+    };
+
+    if client_fd < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        let ip_u32 = u32::from_be(client_addr.sin_addr.s_addr);
+        let ip = std::net::Ipv4Addr::from(ip_u32);
+        let port = u16::from_be(client_addr.sin_port);
+        Ok((client_fd, format!("{}:{}", ip, port)))
+    }
+}
+
+#[cfg(unix)]
+pub fn connect_socket(fd: RawSocket, ip: &str, port: u16) -> io::Result<()> {
+    let ip_addr: std::net::Ipv4Addr = ip
+        .parse()
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid IP format"))?;
+
+    let mut sockaddr: libc::sockaddr_in = unsafe { std::mem::zeroed() };
+    sockaddr.sin_family = libc::AF_INET as libc::sa_family_t;
+    sockaddr.sin_port = port.to_be();
+    sockaddr.sin_addr = libc::in_addr {
+        s_addr: u32::from(ip_addr).to_be(),
+    };
+
+    let res = unsafe {
+        libc::connect(
+            fd,
+            &sockaddr as *const libc::sockaddr_in as *const libc::sockaddr,
+            std::mem::size_of::<libc::sockaddr_in>() as libc::socklen_t,
+        )
+    };
+
+    if res < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+#[cfg(unix)]
+pub fn send_bytes(fd: RawSocket, buf: &[u8]) -> io::Result<usize> {
+    let res = unsafe { libc::send(fd, buf.as_ptr() as *const libc::c_void, buf.len(), 0) };
+    if res < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(res as usize)
+    }
+}
+
+#[cfg(unix)]
+pub fn recv_bytes(fd: RawSocket, buf: &mut [u8]) -> io::Result<usize> {
+    let res = unsafe { libc::recv(fd, buf.as_mut_ptr() as *mut libc::c_void, buf.len(), 0) };
+    if res < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(res as usize)
+    }
+}
+
+// ==================== WINDOWS IMPLEMENTATION ====================
+
 #[cfg(windows)]
 pub type RawSocket = usize;
 
@@ -50,11 +160,32 @@ struct WSADATA {
 }
 
 #[cfg(windows)]
+#[repr(C)]
+struct IN_ADDR {
+    s_addr: u32,
+}
+
+#[cfg(windows)]
+#[repr(C)]
+struct SOCKADDR_IN {
+    sin_family: i16,
+    sin_port: u16,
+    sin_addr: IN_ADDR,
+    sin_zero: [i8; 8],
+}
+
+#[cfg(windows)]
 #[link(name = "ws2_32")]
-extern "system" {
+unsafe extern "system" {
     fn WSAStartup(wVersionRequested: u16, lpWSAData: *mut WSADATA) -> i32;
     fn socket(af: i32, type_: i32, protocol: i32) -> RawSocket;
     fn closesocket(s: RawSocket) -> i32;
+    fn bind(s: RawSocket, name: *const SOCKADDR_IN, namelen: i32) -> i32;
+    fn listen(s: RawSocket, backlog: i32) -> i32;
+    fn accept(s: RawSocket, addr: *mut SOCKADDR_IN, addrlen: *mut i32) -> RawSocket;
+    fn connect(s: RawSocket, name: *const SOCKADDR_IN, namelen: i32) -> i32;
+    fn send(s: RawSocket, buf: *const i8, len: i32, flags: i32) -> i32;
+    fn recv(s: RawSocket, buf: *mut i8, len: i32, flags: i32) -> i32;
 }
 
 #[cfg(windows)]
@@ -79,5 +210,112 @@ pub fn create_tcp_socket() -> io::Result<RawSocket> {
 pub fn close_socket(sock: RawSocket) {
     unsafe {
         closesocket(sock);
+    }
+}
+
+#[cfg(windows)]
+pub fn bind_socket(sock: RawSocket, ip: &str, port: u16) -> io::Result<()> {
+    let ip_addr: std::net::Ipv4Addr = ip
+        .parse()
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid IP format"))?;
+
+    let sockaddr = SOCKADDR_IN {
+        sin_family: AF_INET as i16,
+        sin_port: port.to_be(),
+        sin_addr: IN_ADDR {
+            s_addr: u32::from(ip_addr).to_be(),
+        },
+        sin_zero: [0; 8],
+    };
+
+    let res = unsafe {
+        bind(
+            sock,
+            &sockaddr as *const SOCKADDR_IN,
+            std::mem::size_of::<SOCKADDR_IN>() as i32,
+        )
+    };
+
+    if res != 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+#[cfg(windows)]
+pub fn listen_socket(sock: RawSocket, backlog: i32) -> io::Result<()> {
+    let res = unsafe { listen(sock, backlog) };
+    if res != 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+#[cfg(windows)]
+pub fn accept_socket(sock: RawSocket) -> io::Result<(RawSocket, String)> {
+    let mut client_addr = unsafe { std::mem::zeroed::<SOCKADDR_IN>() };
+    let mut addr_len = std::mem::size_of::<SOCKADDR_IN>() as i32;
+
+    let client_sock = unsafe { accept(sock, &mut client_addr as *mut SOCKADDR_IN, &mut addr_len) };
+
+    if client_sock == INVALID_SOCKET {
+        Err(io::Error::last_os_error())
+    } else {
+        let ip_u32 = u32::from_be(client_addr.sin_addr.s_addr);
+        let ip = std::net::Ipv4Addr::from(ip_u32);
+        let port = u16::from_be(client_addr.sin_port);
+        Ok((client_sock, format!("{}:{}", ip, port)))
+    }
+}
+
+#[cfg(windows)]
+pub fn connect_socket(sock: RawSocket, ip: &str, port: u16) -> io::Result<()> {
+    let ip_addr: std::net::Ipv4Addr = ip
+        .parse()
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid IP format"))?;
+
+    let sockaddr = SOCKADDR_IN {
+        sin_family: AF_INET as i16,
+        sin_port: port.to_be(),
+        sin_addr: IN_ADDR {
+            s_addr: u32::from(ip_addr).to_be(),
+        },
+        sin_zero: [0; 8],
+    };
+
+    let res = unsafe {
+        connect(
+            sock,
+            &sockaddr as *const SOCKADDR_IN,
+            std::mem::size_of::<SOCKADDR_IN>() as i32,
+        )
+    };
+
+    if res != 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(())
+    }
+}
+
+#[cfg(windows)]
+pub fn send_bytes(sock: RawSocket, buf: &[u8]) -> io::Result<usize> {
+    let res = unsafe { send(sock, buf.as_ptr() as *const i8, buf.len() as i32, 0) };
+    if res < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(res as usize)
+    }
+}
+
+#[cfg(windows)]
+pub fn recv_bytes(sock: RawSocket, buf: &mut [u8]) -> io::Result<usize> {
+    let res = unsafe { recv(sock, buf.as_mut_ptr() as *mut i8, buf.len() as i32, 0) };
+    if res < 0 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(res as usize)
     }
 }
